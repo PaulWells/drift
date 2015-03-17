1dd5ff359f1caa1ae5dd662d71131ec68ec78807
Treen Harold Willis <9hwt@linux1.caslab.queensu.ca>
2015-03-16 22:27:17 -0400

    Adds folder from phase 2 submission

---
+++ phase2/unit_tests/parser_tests/parser_specs.rb
@@ -0,0 +1,76 @@
+RSpec.describe "Parser Tests" do
+	before :all do
+		Dir.chdir "#{ENV['HOME']}/Zdrive/cisc458/ptsrc"
+	end
+
+	it 'adds the extern keyword' do
+		actual_output = fetch_actual('addsExtern')
+		expect(actual_output).to match('.sExtern')
+	end
+
+	it 'adds let declarations' do
+		compare_outputs('addsLet')
+	end
+
+	it 'adds var declarations' do
+		compare_outputs('addsVar')
+	end
+
+	it 'adds type declarations' do
+		compare_outputs('addsType')
+	end
+	
+	it 'tests simple if statements' do
+		compare_outputs('simpleIf')
+	end
+
+	it 'updates the if syntax' do
+		compare_outputs('updatesIf')
+	end
+
+	it 'adds elsif syntax' do
+		compare_outputs('addsElsif')
+	end
+
+	it 'updates routine' do
+		compare_outputs('updatesRoutine')
+	end
+
+	it 'has public routines' do
+		compare_outputs('publicRoutines')
+	end
+
+	it 'adds double elsif syntax' do
+		compare_outputs('addsDoubleElsif')
+	end
+	
+	it 'adds nested if' do
+		compare_outputs('addsNestedIf')
+	end	
+	
+	it 'adds modules' do
+		compare_outputs('addsModule')
+	end
+
+	it 'adds loops' do
+		compare_outputs('addsLoop')
+	end
+
+	it 'changes syntax' do
+		compare_outputs('changesSyntax')
+	end
+end
+
+def fetch_actual(test_case)
+	`ssltrace \"ptc -t2 -o2 -L lib/pt unit_tests/parser_tests/#{test_case}.pt\" parser/parser.def -e`.gsub(' ', '').chomp
+end
+
+def fetch_expected(test_case)
+	`cat unit_tests/parser_output_e/#{test_case}`.gsub(' ', '').chomp
+end
+
+def compare_outputs(test_case)
+	actual_output = fetch_actual(test_case)
+	expected_output = fetch_expected(test_case)
+	expect(actual_output).to eq(expected_output)
+end
